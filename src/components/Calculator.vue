0a66266e69392601e1e79e22ab3e2f000dd54d60
wassym-kalouache <wassym.kalouache@dailymotion.com>
2020-03-30 15:15:34 +0200

    FirstCommit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="calculator">
+    <div class="display">0.1221</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">/</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.calculator{
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+
+.display{
+  grid-column: 1/5;
+  background-color: black;
+  color: white;
+}
+
+.zero{
+  grid-column: 1/3;
+}
+
+.btn{
+  background-color: #eee;
+  border: 1px solid #333;
+}
+
+.operator{
+  background-color: orange;
+  color: white;
+}
+
+</style>
