359d8992bbb41c338573bed88dd5122238e3520f
wassym-kalouache <wassym.kalouache@dailymotion.com>
2020-03-30 14:18:43 +0200

    Fist component

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Premier Titre</h1>
+        <h2>Second Titre</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
